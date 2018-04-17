8a56a019eb575247960ef06a5b1be496f799e451
Robert Paul Trettenero <paul.trettenero@petrocloud.com>
2018-04-17 18:33:57 -0500

    Added ship movement

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,14 +5,32 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    [Tooltip("In ms^-1")] [SerializeField] float speed = 13f;
+    [Tooltip("In m")] [SerializeField] float xRange = 7.25f;
+
+    void Start () {
 		
 	}
 	
-	// Update is called once per frame
 	void Update () {
-        float horizontalThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        print(horizontalThrow); // This is slowly moving away from +/- 1
+        ProcessTranslation();
+        ProcessRotation();      // Lets get the ship to rotate sidewys
 	}
+
+    private void ProcessTranslation()
+    {
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal"); // This is slowly moving away from +/- 1
+        float yThrow = CrossPlatformInputManager.GetAxis("Vertical"); // This is slowly moving away from +/- 1
+        float xOffset = xThrow * speed * Time.deltaTime;
+        float yOffset = yThrow * speed * Time.deltaTime;
+        float rawXPos = transform.localPosition.x + xOffset;
+        float rawYPos = transform.localPosition.y + yOffset;
+
+        transform.localPosition = new Vector3(Mathf.Clamp(rawXPos, -xRange, xRange), Mathf.Clamp(rawYPos, -5f, 4.5f), transform.localPosition.z);
+    }
+
+    private void ProcessRotation()
+    {
+        transform.localRotation = Quaternion.Euler(90f, 0f, 0f); // in our case x is pitch y is yaw z is roll
+    }
 }
