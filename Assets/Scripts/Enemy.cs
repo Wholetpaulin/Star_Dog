bbdfd0f0664368ffe35a804c44b23be0894de464
Robert Paul Trettenero <tretteneropaul@yahoo.com>
2018-05-07 18:27:54 -0500

    Added some env changes to make more interesting

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,6 +8,7 @@ public class Enemy : MonoBehaviour {
     [SerializeField] Transform parent;
     ScoreBoard scoreBoard;
     [SerializeField] int scoreHit = 10;    // How much the score goes up each time
+    [SerializeField] int hits = 3;
 
     void Start () {
         AddNonTriggerBoxCollider();
@@ -23,6 +24,16 @@ public class Enemy : MonoBehaviour {
     private void OnParticleCollision(GameObject other)
     {
         scoreBoard.ScoreHit(scoreHit);
+        //todo: consider hit FX
+        hits--;
+        if (hits <= 0)
+        {
+            KillEnemy();
+        }
+    }
+
+    private void KillEnemy()
+    {
         GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity); // drop a death FX prefab in the worl
         fx.transform.parent = parent;
         Destroy(gameObject);
